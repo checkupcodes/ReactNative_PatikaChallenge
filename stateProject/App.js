eb956bb5980acf275d90e2ba5162357bbf975e1c
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-15 15:14:10 +0300

    v0.3.2 - State finish
    Switch butonu ile sadece isseleceted leri ekranda göstertemye yarayan yapıyı kullandık

--- stateProject/App.js
+++ stateProject/App.js
@@ -1,15 +1,35 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Text, View, Button} from 'react-native';
+import {SafeAreaView, Text, View, Button, Switch, FlatList} from 'react-native';
 
+const data = [
+  {id: 0, name: 'cafe.exe', isFavorite: true},
+  {id: 1, name: 'cekap', isFavorite: false},
+  {id: 2, name: 'checkupcodes', isFavorite: false},
+  {id: 3, name: 'elselif', isFavorite: true},
+  {id: 4, name: 'cekap.exe', isFavorite: true},
+  {id: 5, name: 'elif.exe', isFavorite: false},
+  {id: 6, name: 'cafe.exe', isFavorite: true},
+];
 function App() {
-  const [counter, setCounter] = useState(0);
-  function updateCounter() {
-    setCounter(counter + 1);
+  const [CafeList, setCafeList] = useState(data);
+  const [showOnlyFavories, setShowOnlyFavories] = useState(false);
+
+  function onFavoritesChange(isFavoriteSelected) {
+    setShowOnlyFavories(isFavoriteSelected);
+    isFavoriteSelected
+      ? setCafeList(CafeList.filter(cafe => cafe.isFavorite))
+      : setCafeList(data);
   }
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 30}}>Counter {counter}</Text>
-      <Button title="uptade button" onPress={updateCounter}></Button>
+      <View>
+        <Text>Show only Favories</Text>
+      </View>
+      <Switch style={{}} value={showOnlyFavories} onValueChange={onFavoritesChange} />
+      <FlatList
+        data={CafeList}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+      />
     </SafeAreaView>
   );
 }
