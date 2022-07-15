680a4395639ae270afd8dbdb8037129e494ef08e
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-15 15:54:42 +0300

    v0.3.21 - useEffect
    use effect konusu işlendi

--- stateProject/App.js
+++ stateProject/App.js
@@ -1,35 +1,30 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, View, Button, Switch, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Button,
+  Switch,
+  FlatList,
+  Alert,
+} from 'react-native';
 
-const data = [
-  {id: 0, name: 'cafe.exe', isFavorite: true},
-  {id: 1, name: 'cekap', isFavorite: false},
-  {id: 2, name: 'checkupcodes', isFavorite: false},
-  {id: 3, name: 'elselif', isFavorite: true},
-  {id: 4, name: 'cekap.exe', isFavorite: true},
-  {id: 5, name: 'elif.exe', isFavorite: false},
-  {id: 6, name: 'cafe.exe', isFavorite: true},
-];
 function App() {
-  const [CafeList, setCafeList] = useState(data);
-  const [showOnlyFavories, setShowOnlyFavories] = useState(false);
-
-  function onFavoritesChange(isFavoriteSelected) {
-    setShowOnlyFavories(isFavoriteSelected);
-    isFavoriteSelected
-      ? setCafeList(CafeList.filter(cafe => cafe.isFavorite))
-      : setCafeList(data);
-  }
+  const [number, setNumber] = useState(0);
+  const [counter, setCounter] = useState(10);
+  useEffect(() => {
+    Alert.alert('number');
+  }, [number]);
+  useEffect(() => {
+    Alert.alert('counter');
+  }, [counter]);
   return (
     <SafeAreaView>
-      <View>
-        <Text>Show only Favories</Text>
-      </View>
-      <Switch style={{}} value={showOnlyFavories} onValueChange={onFavoritesChange} />
-      <FlatList
-        data={CafeList}
-        renderItem={({item}) => <Text>{item.name}</Text>}
-      />
+      <Text>Button click</Text>
+      <Text>Number : {number}</Text>
+      <Text>Counter : {counter}</Text>
+      <Button title="Click" onPress={() => setNumber(number + 1)} />
+      <Button title="Update Counter" onPress={() => setCounter(counter + 10)} />
     </SafeAreaView>
   );
 }
