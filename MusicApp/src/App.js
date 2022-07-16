79308bb21e9554da33e3393c99f7ad191e820c9d
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-16 16:23:56 +0300

    v0.3.6 - MUSICAPP continue
    -  Add serachbar

--- MusicApp/src/App.js
+++ MusicApp/src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, SafeAreaView, FlatList, StyleSheet} from 'react-native';
 import song_data from './components/songCards_data.json';
 import SongCard from './components/SongCard/SongCard';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
+  const [list, setList] = useState(song_data);
   const renderSong = ({item}) => <SongCard song={item} />;
   const itemSeperatorItem = () => <View style={styles.seperator}></View>;
+  const handleSearch = text => {
+    const filteredList = song_data.filter(song => {
+      const searcedText = text.toLowerCase();
+      const currentTitle = song.title.toLowerCase();
+
+      return currentTitle.indexOf(searcedText) > -1;
+    });
+    setList(filteredList);
+  };
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <FlatList
-          keyExtractor={item => item.id}
-          data={song_data}
-          renderItem={renderSong}
-          ItemSeparatorComponent={itemSeperatorItem}
-        />
-      </View>
+      <SearchBar onSearch={handleSearch} />
+      <FlatList
+        keyExtractor={item => item.id}
+        data={list}
+        renderItem={renderSong}
+        ItemSeparatorComponent={itemSeperatorItem}
+      />
     </SafeAreaView>
   );
 }
