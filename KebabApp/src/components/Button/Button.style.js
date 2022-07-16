f5b55d6a441ef4b0bfad9d70b62802900ed143c9
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-16 21:01:13 +0300

    v0.4.52 - Buton Component ekleni
    -Componens : Button

---
+++ KebabApp/src/components/Button/Button.style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: '#1976d2',
+    borderRadius: 10,
+    margin: 5,
+  },
+  text: {
+    color: 'white',
+    padding: 8,
+    fontWeight: '800',
+    textAlign: 'center',
+  },
+});
--- KebabApp/src/pages/WelcomeScreen.js
+++ KebabApp/src/pages/WelcomeScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {Text} from 'react-native';
 
+import Button from '../components/Button/Button';
+
 function Welcome() {
   return (
     <SafeAreaView>
       <Text>Welcome</Text>
+      <Button text="Submit" onPress={null} />
     </SafeAreaView>
   );
 }
