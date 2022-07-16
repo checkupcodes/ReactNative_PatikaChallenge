0a2620e7311b86ba6bc29cd1685082c91f070748
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-16 19:48:37 +0300

    v0.4.13 - Switch Pages
    - Sayfalar arasında geçiş yapabilme

--- Navigation/src/pages/First.js
+++ Navigation/src/pages/First.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-const First = () => {
+const First = props => {
+  function navigateToPage() {
+    props.navigation.navigate('SecondPage');
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}>Hi from cekap</Text>
+      <Button title="Go to second" onPress={navigateToPage} />
     </SafeAreaView>
   );
 };
--- Navigation/src/pages/Second.js
+++ Navigation/src/pages/Second.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
-const Second = () => {
+function Second(props) {
+  function backToPage() {
+    props.navigation.goBack();
+  }
   return (
-    <SafeAreaView>
-      <Text>Hi from cekap</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hi from cekap</Text>
+      <Button title="Go to back" onPress={backToPage} />
     </SafeAreaView>
   );
-};
+}
 
 export default Second;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  text: {fontSize: 40, fontWeight: '800'},
+});
