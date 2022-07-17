be589d62717031ad18d1d2f1ec108bc4ea1e8508
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-17 16:27:37 +0300

    v0.6.1 - Axios ekleme ve yükleme

---
+++ axios/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import axios from 'axios';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Selam</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
