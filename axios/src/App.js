885a23e879326d256ec2504e78bb475b06108bab
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-17 17:51:12 +0300

    v0.6.11 - Axios button ve kurulum (sync))

--- axios/src/App.js
+++ axios/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {Button, SafeAreaView, Text, View} from 'react-native';
 import axios from 'axios';
 
 function App() {
+  async function fetchData() {
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+  }
   return (
     <SafeAreaView>
       <View>
         <Text>Selam</Text>
+        <Button title="Fetch Data" onPress={fetchData} />
       </View>
     </SafeAreaView>
   );
 }
+
+export default App;
