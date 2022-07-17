9f49ac5eddc3f5300c81c3fe84f3308cf1832b56
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-17 13:32:37 +0300

    v0.4.56 - Sayfaya state giydirmesi
    - SingIn sayfasına state giydirdik

--- KebabApp/src/pages/SignIn.js
+++ KebabApp/src/pages/SignIn.js
@@ -1,21 +1,57 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Alert} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Button from '../components/Button';
 
 import Input from '../components/Input/Input';
 
 function SingIn() {
+  const [username, setUserName] = useState(null);
+  const [surname, setSurname] = useState(null);
+  const [age, setAge] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [from, setFrom] = useState(null);
+
+  function handleSubmit() {
+    if (!username || !surname || !age || !email || !from) {
+      Alert.alert('Mac Fitness' , 'You left an empty value');
+    }
+
+    const user = {
+      username,
+      surname,
+      age,
+      email,
+      from,
+    };
+  }
+
   return (
     <SafeAreaView>
       <Text>Sign in</Text>
       <View style={{margin: 50}} />
-      <Input label="Name" placeholder="Write your name" />
-      <Input label="Surname" placeholder="Write your surname" />
-      <Input label="Age" placeholder="Write your age" />
-      <Input label="Email" placeholder="Write your email" />
-      <Input label="From" placeholder="Where are you from" />
-      <Button text="Submit" onPress={null} />
+      <Input
+        label="Name"
+        placeholder="Write your name"
+        onChangeText={setUserName}
+      />
+      <Input
+        label="Surname"
+        placeholder="Write your surname"
+        onChangeText={setSurname}
+      />
+      <Input label="Age" placeholder="Write your age" onChangeText={setAge} />
+      <Input
+        label="Email"
+        placeholder="Write your email"
+        onChangeText={setEmail}
+      />
+      <Input
+        label="From"
+        placeholder="Where are you from"
+        onChangeText={setFrom}
+      />
+      <Button text="Submit" onPress={handleSubmit} />
     </SafeAreaView>
   );
 }
