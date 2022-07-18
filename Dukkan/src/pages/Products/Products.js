843f0af48b730d9602ebabee4cf46bb1e621daa0
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-18 15:03:24 +0300

    v0.7.18 - VERİLERİ ÇEKME (y)

--- Dukkan/src/pages/Products/Products.js
+++ Dukkan/src/pages/Products/Products.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlatList, SafeAreaView, Text, View} from 'react-native';
 import styles from './Products.style';
+import axios from 'axios';
 
 import Config from 'react-native-config';
 
 const Products = () => {
-  const renderProduct = ({item}) => null;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const {data: productData} = await axios.get(Config.API_URL);
+    setData(productData);
+  };
+
+  const renderProduct = ({item}) => <Text>{item.title}</Text>;
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <FlatList data={null} renderItem={renderProduct} />
+        <FlatList data={data} renderItem={renderProduct} />
       </View>
     </SafeAreaView>
   );
