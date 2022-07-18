feef3ef6e920cf17baa636937bb220b4edc3b87a
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-18 14:24:48 +0300

    v0.7.151 - Config kontrol
    Kontrol çalıştı

--- Dukkan/src/pages/Products/Products.js
+++ Dukkan/src/pages/Products/Products.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import styles from './Products.style';
 
+import Config from 'react-native-config';
+
 const Products = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>Hi Products</Text>
+        <Text>Hi Products {Config.API_URL}</Text>
       </View>
     </SafeAreaView>
   );
