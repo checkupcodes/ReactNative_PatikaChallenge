833abd1e3d3f95fc436237e0a477fb9a4cd2ae12
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-07-19 12:09:43 +0300

    v0.7.27 - Veri çekiliyor details için

--- Dukkan/src/pages/Details/Details.js
+++ Dukkan/src/pages/Details/Details.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text, View, Image} from 'react-native';
+import {SafeAreaView, Text, View, Image, ActivityIndicator} from 'react-native';
+import Config from 'react-native-config';
+import useFetch from '../../hooks/useFetch';
 import styles from './Details.style';
 
 const Details = ({route}) => {
   const {id} = route.params;
+  const {loading, error, data} = useFetch(`${Config.API_URL}/${id}`);
   console.log(id);
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
   return (
     <SafeAreaView style={styles.container}>
       <Image style={styles.image} />
